718762446f0f14e3c598d737415343cec79d980e
hezhijie0327 <hezhijie0327@hotmail.com>
2026-01-24 02:00:03 +0000

    Synced by GitHub Actions

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -12,6 +12,7 @@ import { Fullscreen } from './fullscreen';
 import { Keyboard } from './keyboard';
 import { Mouse } from './mouse';
 import { Recorder } from './recorder';
+import { Screenshot } from './screenshot';
 import { SerialPort } from './serial-port';
 import { Settings } from './settings';
 import { Video } from './video';
@@ -125,6 +126,7 @@ export const Menu = () => {
               </>
             )}
 
+            <Screenshot />
             <Recorder />
 
             <Divider type="vertical" className="px-0.5" />
